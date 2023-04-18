fac8790ca6b6e26e40ab29be818a28d0bd1cec75
liftoachgolf <114295764+liftoachgolf@users.noreply.github.com>
2023-04-18 19:51:31 +0300

    Add files via upload

---
+++ logging.go
@@ -0,0 +1,88 @@
+package logging //льет логи и в аутапут и в файл
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, w := range hook.Writer {
+		w.Write([]byte(line))
+	}
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) GetLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
+}
+
+func init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.MkdirAll("logs", 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	if err != nil {
+		panic(err)
+	}
+
+	l.SetOutput(io.Discard)
+
+	l.AddHook(&writerHook{
+		Writer:    []io.Writer{allFile, os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	l.SetLevel(logrus.TraceLevel)
+
+	e = logrus.NewEntry(l)
+}
+
+//info.log -- info, warning, error, critical
+//debug.log -- info, warning, error, critical, debug
+//trace.log -- info, warning, error, critical, debug, trace
+//error.log -- error, critical
+
+//kafka -- info, debug
+//file -- error, trace
+//stdout-- warning, critical
